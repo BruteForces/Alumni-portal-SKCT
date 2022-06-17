f6e137c4de38e9b85fa9ad389e4178e8cc94f058
Mohamed Aslam <mohamedaslammn.2003@gmail.com>
2022-06-17 11:27:18 +0530

    Auto Scroll

--- src/pages/Events/Events.js
+++ src/pages/Events/Events.js
@@ -1,9 +1,21 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import EventCard from "components/EventComponents/EventCard";
 import styles from "./Events.module.css";
 
 const Events = () => {
   const [isActive, setIsActive] = useState(false);
+  const eventsRef = useRef(null);
+
+  useEffect(() => {
+    const scrollWidth = eventsRef.current.scrollWidth;
+
+    const interval = setInterval(() => {
+      if (eventsRef.current.scrollLeft !== scrollWidth) {
+        eventsRef.current.scrollTo(eventsRef.current.scrollLeft + 1, 0);
+      }
+    }, 15);
+    return () => clearInterval(interval);
+  }, []);
   return (
     <div className={styles["event_page"]}>
       <div className={styles["event_body"]}>
@@ -13,12 +25,12 @@ const Events = () => {
               <span>U</span>PCOMING <span>E</span>VENTS
             </p>
           </div>
-          <div className={styles["events"]}>
+          <div ref={eventsRef} className={styles["events"]}>
             {[...Array(10).keys()].map((event, idx) => (
               <EventCard
                 id={event}
                 active={idx === 0}
-                scrollSnap={idx % 3 === 0}
+                // scrollSnap={idx % 3 === 0}
               />
             ))}
           </div>
